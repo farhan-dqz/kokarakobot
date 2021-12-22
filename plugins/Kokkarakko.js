58909b5333c1187a559ce0502e3fb39350b7f495
Farhan_Dqz <83164448+farhan-dqz@users.noreply.github.com>
2021-12-22 00:42:07 -0800

    Update Kokkarakko.js

--- plugins/Kokkarakko.js
+++ plugins/Kokkarakko.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 Asena.addCommand({ on: 'text', fromMe: false,onlyPm: true }, (async (message, match) => {
 
     var r_text = new Array();
-    r_text[0] = "https://i.imgur.com/cNpMNKX.jpeg";
+    r_text[0] = Config.IMG;
     var i = Math.floor(1 * Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
@@ -19,32 +19,32 @@ Asena.addCommand({ on: 'text', fromMe: false,onlyPm: true }, (async (message, ma
     let regexb6Julie = new RegExp('u')
 
     if (regexb1Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
     else if (regexb2Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
     else if (regexb3Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
     else if (regexb4Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
     else if (regexb5Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
     else if (regexb6Julie.test(message.message)) {
-        await message.sendMessage(Buffer(respoimage.data), MessageType.image, {
+        await message.sendMessage(Buffer.from(respoimage.data), MessageType.image, {
         mimetype: Mimetype.png, caption: Config.TEXT})
         
     }
