bd3d04a3b4085ef28b76aa0ddf83e127d71c1e8e
Farhan_Dqz <kaztrobabalover@gmail.com>
2021-12-21 20:57:34 -0800

    efd

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -8,7 +8,6 @@ WhatsJulie - Yusuf Usta
 const Julie = require('../events');
 const Config = require('../config');
 const Heroku = require('heroku-client');
-const {secondsToHms} = require('./afk');
 const got = require('got');
 const {MessageType} = require('@adiwajshing/baileys');
 const sql = require('./sql/greetings');
@@ -182,73 +181,6 @@ Julie.addCommand({pattern: 'shutdown$', fromMe: true, dontAddCommandList: true, 
     });
 }));
 
-
-if (Config.WORKTYPE == 'private') {
-
-    Julie.addCommand({pattern: 'dyno$', fromMe: true, dontAddCommandList: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
-
-        heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
-            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
-            headers = {
-                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
-                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-            }
-            await got(url, {headers: headers}).then(async (res) => {
-               const resp = JSON.parse(res.body);
-               total_quota = Math.floor(resp.account_quota);
-               quota_used = Math.floor(resp.quota_used);         
-               percentage = Math.round((quota_used / total_quota) * 100);
-               remaining = total_quota - quota_used;
-               await message.client.sendMessage(
-                    message.jid,
-                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
-                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
-                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
-                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text
-               );
-            }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
-            });        
-        });
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
-
-    Julie.addCommand({pattern: 'dyno$', fromMe: false, dontAddCommandList: true, desc: Lang.DYNO_DESC}, (async (message, match) => {
-
-        heroku.get('/account').then(async (account) => {
-            // have encountered some issues while calling this API via heroku-client
-            // so let's do it manually
-            url = "https://api.heroku.com/accounts/" + account.id + "/actions/get-quota"
-            headers = {
-                "User-Agent": "Chrome/80.0.3987.149 Mobile Safari/537.36",
-                "Authorization": "Bearer " + Config.HEROKU.API_KEY,
-                "Accept": "application/vnd.heroku+json; version=3.account-quotas",
-            }
-            await got(url, {headers: headers}).then(async (res) => {
-               const resp = JSON.parse(res.body);
-               total_quota = Math.floor(resp.account_quota);
-               quota_used = Math.floor(resp.quota_used);         
-               percentage = Math.round((quota_used / total_quota) * 100);
-               remaining = total_quota - quota_used;
-               await message.client.sendMessage(
-                    message.jid,
-                    Lang.DYNO_TOTAL + ": ```{}```\n\n".format(secondsToHms(total_quota))  + 
-                    Lang.DYNO_USED + ": ```{}```\n".format(secondsToHms(quota_used)) +  
-                    Lang.PERCENTAGE + ": ```{}```\n\n".format(percentage) +
-                    Lang.DYNO_LEFT + ": ```{}```\n".format(secondsToHms(remaining)),
-                    MessageType.text
-               );
-            }).catch(async (err) => {
-                await message.client.sendMessage(message.jid,err.message, MessageType.text);     
-            });        
-        });
-    }));
-}
 
 Julie.addCommand({pattern: 'setvar ?(.*)', fromMe: true, dontAddCommandList: true, desc: Lang.SETVAR_DESC}, (async(message, match) => {
 
